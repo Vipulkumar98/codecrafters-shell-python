23b575681a384fc70148742a4b4a1e830db6e5c0
Vipulkumar98 <vipulkumarbahilam@gmail.com>
2026-01-18 20:28:59 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,11 +7,45 @@ def type_command(cmd):
         return
     path_env = os.environ.get("PATH", "")
     for directory in path_env.split(os.pathsep):
+        if not directory:
+            continue
         full_path = os.path.join(directory, cmd)
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
             print(f"{cmd} is {full_path}")
             return
     print(f"{cmd}: not found")
+
+
+def find_executable(cmd):
+    path_env = os.environ.get("PATH", "")
+    for directory in path_env.split(os.pathsep):
+        if not directory:
+            continue
+        full_path = os.path.join(directory, cmd)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return full_path
+    return None
+
+def run_external(cmd, args):
+    executable = find_executable(cmd)
+
+    if executable is None:
+        print(f"{cmd}: command not found")
+        return
+
+    pid = os.fork()
+
+    if pid == 0:
+        # Child process
+        try:
+            os.execv(executable, [cmd] + args)
+        except Exception as e:
+            print(f"exec error: {e}", file=sys.stderr)
+            os._exit(1)
+    else:
+        # Parent process waits
+        os.waitpid(pid, 0)
+
 
 BUILTINS = {
     "type": type_command,
@@ -23,19 +57,26 @@ def main():
     # Uncomment this block to pass the first stage
 
     while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
+        try:
 
-        # Wait for user input
-        command = input().split()
-        cmd = command[0]
-        args = command[1:]
+            sys.stdout.write("$ ")
+            sys.stdout.flush()
 
-        if cmd in BUILTINS:
-            BUILTINS[cmd](*args)
-        else:
-            print(f"{cmd}: command not found")
+            # Wait for user input
+            command = input().split()
+            cmd = command[0]
+            args = command[1:]
 
+            if cmd in BUILTINS:
+                BUILTINS[cmd](*args)
+            else:
+                # print(f"{cmd}: command not found")
+                run_external(cmd, args)
+        except EOFError:
+            print()
+            break
+        except KeyboardInterrupt:
+            print()
 
 
 if __name__ == "__main__":
