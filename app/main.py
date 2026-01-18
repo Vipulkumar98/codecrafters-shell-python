d63f52b5410855ba4d9a8c225d0b57897a8cfc1a
Vipulkumar98 <vipulkumarbahilam@gmail.com>
2026-01-18 19:02:17 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,24 +1,28 @@
 import sys
 
 
+BUILTINS = {
+    "type": lambda x: print(f"{x} is a shell builtin" if x in BUILTINS else f"{x}: not found"),
+    "echo": lambda *args: print(" ".join(args)),
+    "exit": lambda code=0, *_: sys.exit(int(code)),
+}
+
 def main():
     # Uncomment this block to pass the first stage
 
     while True:
         sys.stdout.write("$ ")
-
+        sys.stdout.flush()
 
         # Wait for user input
-        command = input().strip()
-        if command == "exit":
-            sys.exit(0)
+        command = input().split()
+        cmd = command[0]
+        args = command[1:]
 
-        if command.startswith("echo "):
-            echo_text = command[5:]
-            print(echo_text)
-            continue
-
-        print(f"{command}: command not found")
+        if cmd in BUILTINS:
+            BUILTINS[cmd](*args)
+        else:
+            print(f"{cmd}: command not found")
 
 
 
