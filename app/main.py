a0219ca1ca2d91502c101f68020ba0f379d51fb0
Vipulkumar98 <vipulkumarbahilam@gmail.com>
2026-01-18 19:35:20 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
 import sys
+import os
 
+def type_command(cmd):
+    if cmd in BUILTINS:
+        print(f"{cmd} is a shell builtin")
+        return
+    path_env = os.environ.get("PATH", "")
+    for directory in path_env.split(os.pathsep):
+        full_path = os.path.join(directory, cmd)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            print(f"{cmd} is {full_path}")
+            return
+    print(f"{cmd}: not found")
 
 BUILTINS = {
-    "type": lambda x: print(f"{x} is a shell builtin" if x in BUILTINS else f"{x}: not found"),
+    "type": type_command,
     "echo": lambda *args: print(" ".join(args)),
     "exit": lambda code=0, *_: sys.exit(int(code)),
 }
